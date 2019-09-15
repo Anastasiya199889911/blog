24ccb0fedca0a3aa6123a98472d7a7cdc6b7db40
Anastasiya <romanenko.anastasiya1998@yandex.ua>
2019-09-15 20:15:24 +0300

    admin

--- Main/admin.py
+++ Main/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+class EventsAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Events._meta.fields]
+
+    class Meta:
+        model = Events
+
+admin.site.register(Events, EventsAdmin)
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in News._meta.fields]
+
+    class Meta:
+        model = News
+
+admin.site.register(News, NewsAdmin)
+
+
+class ArticlesAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Articles._meta.fields]
+
+    class Meta:
+        model = Articles
+
+admin.site.register(Articles, ArticlesAdmin)
+
+
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Reviews._meta.fields]
+
+    class Meta:
+        model = Reviews
+
+admin.site.register(Reviews, ReviewsAdmin)
--- Main/models.py
+++ Main/models.py
@@ -1,48 +1,48 @@
 from django.db import models
 
-# class Articles(models.Model):
-#     text = models.CharField(max_length=500, blank=True, null=True, verbose_name='Текст')
-#     name = models.CharField(max_length=100, blank=True, null=True, verbose_name='Заголовок')
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'articles'
-#         verbose_name_plural = 'Статьи'
-#
-#
-# class Events(models.Model):
-#     name = models.CharField(max_length=200, blank=True, null=True, verbose_name='Заголовок')
-#     date_start = models.DateField(blank=True, null=True, verbose_name='Дата начала')
-#     date_end = models.DateField(blank=True, null=True, verbose_name='Дата конца')
-#     place_city = models.CharField(max_length=50, blank=True, null=True, verbose_name='Город')
-#     place_country = models.CharField(max_length=50, blank=True, null=True, verbose_name='Страна')
-#     text = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Текст')
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'events'
-#         verbose_name_plural = 'Уже скоро'
-#
-#
-# class News(models.Model):
-#     name = models.CharField(max_length=100, blank=True, null=True, verbose_name='Заголовок')
-#     text = models.CharField(max_length=500, blank=True, null=True, verbose_name='Текст')
-#     img_path = models.TextField(blank=True, null=True, verbose_name='Изображение')
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'news'
-#         verbose_name_plural = 'Новости'
-#
-#
-# class Reviews(models.Model):
-#     name = models.CharField(max_length=50, blank=True, null=True, verbose_name='Имя')
-#     city = models.CharField(max_length=50, blank=True, null=True, verbose_name='Город')
-#     country = models.CharField(max_length=50, blank=True, null=True, verbose_name='Страна')
-#     text = models.CharField(max_length=500, blank=True, null=True, verbose_name='Текст')
-#     position = models.CharField(max_length=100, blank=True, null=True, verbose_name='Должность')
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'reviews'
-#         verbose_name_plural = 'Отзывы'
+class Articles(models.Model):
+    text = models.CharField(max_length=500, blank=True, null=True, verbose_name='Текст')
+    name = models.CharField(max_length=100, blank=True, null=True, verbose_name='Заголовок')
+
+    class Meta:
+        managed = False
+        db_table = 'articles'
+        verbose_name_plural = 'Статьи'
+
+
+class Events(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True, verbose_name='Заголовок')
+    date_start = models.DateField(blank=True, null=True, verbose_name='Дата начала')
+    date_end = models.DateField(blank=True, null=True, verbose_name='Дата конца')
+    place_city = models.CharField(max_length=50, blank=True, null=True, verbose_name='Город')
+    place_country = models.CharField(max_length=50, blank=True, null=True, verbose_name='Страна')
+    text = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Текст')
+
+    class Meta:
+        managed = False
+        db_table = 'events'
+        verbose_name_plural = 'Уже скоро'
+
+
+class News(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True, verbose_name='Заголовок')
+    text = models.CharField(max_length=500, blank=True, null=True, verbose_name='Текст')
+    img_path = models.TextField(blank=True, null=True, verbose_name='Изображение')
+
+    class Meta:
+        managed = False
+        db_table = 'news'
+        verbose_name_plural = 'Новости'
+
+
+class Reviews(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True, verbose_name='Имя')
+    city = models.CharField(max_length=50, blank=True, null=True, verbose_name='Город')
+    country = models.CharField(max_length=50, blank=True, null=True, verbose_name='Страна')
+    text = models.CharField(max_length=500, blank=True, null=True, verbose_name='Текст')
+    position = models.CharField(max_length=100, blank=True, null=True, verbose_name='Должность')
+
+    class Meta:
+        managed = False
+        db_table = 'reviews'
+        verbose_name_plural = 'Отзывы'
